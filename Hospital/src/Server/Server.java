34e791b8e0a9a4654184d97aa8e2acb93fd95ade
eebus <simpan.svensson@gmail.com>
2015-02-20 15:45:04 +0100

    Added runnable server functionality

---
+++ Hospital/src/Server/Server.java
@@ -0,0 +1,96 @@
+package Server;
+import java.io.*;
+import java.net.*;
+import java.security.KeyStore;
+
+import javax.net.*;
+import javax.net.ssl.*;
+import javax.security.cert.X509Certificate;
+
+public class Server implements Runnable {
+    private ServerSocket serverSocket = null;
+    private static int numConnectedClients = 0;
+    private Login log;
+
+    public Server(Login log) throws IOException {
+    	 int port = 9876;
+    	 
+         String type = "TLS";
+    	ServerSocketFactory ssf = Server.getServerSocketFactory(type);
+    	serverSocket = ssf.createServerSocket(port);
+        ((SSLServerSocket)serverSocket).setNeedClientAuth(true); // enables client authentication;
+        newListener();
+        this.log=log;
+        
+    }
+
+    public void run() {
+        try {
+            SSLSocket socket=(SSLSocket)serverSocket.accept();
+            newListener();
+            SSLSession session = socket.getSession();
+            X509Certificate cert = (X509Certificate)session.getPeerCertificateChain()[0];
+            String subject = cert.getSubjectDN().getName();
+    	    numConnectedClients++;
+            System.out.println("client connected");
+            System.out.println("client name (cert subject DN field): " + subject);
+            System.out.println(numConnectedClients + " concurrent connection(s)\n");
+
+            PrintWriter out = null;
+            BufferedReader in = null;
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String clientMsg = null;
+            while ((clientMsg = in.readLine()) != null) {
+			    String rev = new StringBuilder(clientMsg).reverse().toString();
+                System.out.println("received '" + clientMsg + "' from client");
+                System.out.print("sending '" + rev + "' to client...");
+				out.println(rev);
+				out.flush();
+                System.out.println("done\n");
+			}
+			in.close();
+			out.close();
+			socket.close();
+    	    numConnectedClients--;
+            System.out.println("client disconnected");
+            System.out.println(numConnectedClients + " concurrent connection(s)\n");
+		} catch (IOException e) {
+            System.out.println("Client died: " + e.getMessage());
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    private void newListener() { (new Thread(this)).start(); } // calls run()
+    public static ServerSocketFactory getServerSocketFactory(String type) {
+        if (type.equals("TLS")) {
+            SSLServerSocketFactory ssf = null;
+            try { // set up key manager to perform server authentication
+                SSLContext ctx = SSLContext.getInstance("TLS");
+                KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+                TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+                KeyStore ks = KeyStore.getInstance("JKS");
+				KeyStore ts = KeyStore.getInstance("JKS");
+                char[] password = "password".toCharArray();
+
+                ks.load(new FileInputStream("serverkeystore"), password);  // keystore password (storepass)
+                ts.load(new FileInputStream("servertruststore"), password); // truststore password (storepass)
+                kmf.init(ks, password); // certificate password (keypass)
+                tmf.init(ts);  // possible to use keystore as truststore here
+                ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+                ssf = ctx.getServerSocketFactory();
+                return ssf;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            return ServerSocketFactory.getDefault();
+        }
+        return null;
+    }
+   
+
+    
+}
--- Hospital/src/Server/ServerMain.java
+++ Hospital/src/Server/ServerMain.java
@@ -1,9 +1,33 @@
 package Server;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.security.KeyStore;
+
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+
 public class ServerMain {
 	
 	public static void main (String []args){
 		Login log = new Login();
+		 
+        System.out.println("\nServer Started\n");
+       
+        try {
+            
+            new Server(log);
+        } catch (IOException e) {
+            System.out.println("Unable to start Server: " + e.getMessage());
+            e.printStackTrace();
+        }
+		    
 	}
+	
 
 }
